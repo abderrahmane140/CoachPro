cd32e7eaa796e860439e2c967b09dec4fa0f19b8
abderrahmane140 <bsarabderrahmane01@gmail.com>
2025-12-17 22:29:29 +0100

    the dashboard for the coach

--- pages/coach/dashboard.php
+++ pages/coach/dashboard.php
@@ -1,36 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>athlete Dashboard</title>
-</head>
-<body>
-<div class="min-h-screen flex">
-  <!-- Sidebar -->
-  <div class="flex-none w-64 bg-gray-800">
-    <div class="space-y-4 px-4 py-6">
-      <!-- Sidebar Logo -->
-      <div class="shrink-0">
-        <h1 class="font-bold text-xl text-white p-1">Coach Pro</h1>
-      </div>
-
-      <!-- Sidebar Links -->
-      <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium bg-gray-950/50 text-white">Dashboard</a>
-      <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Availability</a>
-      <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Bookings</a>
-      <!-- <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
-      <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Reports</a> -->
-    </div>
-
-    <!-- Footer (Optional, can add more links here) -->
-    <div class="mt-auto px-4 py-6">
-      <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Profile</a>
-      <a href="#" class="block px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Sign Out</a>
-    </div>
-  </div>
-
+<?php include '../../template/coach_header.php' ?>
   
   <!-- Main Content -->
   <div class="flex-1 bg-gray-900 px-6 py-8">
@@ -55,7 +23,6 @@
       </div>
     </div>
   </div>
-</div>
 
-</body>
-</html>
+
+<?php include '../../template/coach_footer.php' ?>
